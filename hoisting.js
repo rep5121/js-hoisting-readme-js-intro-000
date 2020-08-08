36a4cf4c7c27c8d02a39b5cb113237ef7406cac6
bold-mixer-0681 <renee.pujol@gmail.com>
2020-08-08 22:26:42 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,35 @@
+
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return "maybe";
 }
+
+
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
+crazy()();
+
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
+  var name = "Kristin";
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
+
+sayMyName()();
